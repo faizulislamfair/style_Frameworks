da88c8b19627309e04f3a0846d74a71a501b8d01
faizulislamfair <faizulislamfair@gmail.com>
2022-06-09 20:32:51 +0600

    tailwind setup

--- tailwind_css/tailwind.config.js
+++ tailwind_css/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}'
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
